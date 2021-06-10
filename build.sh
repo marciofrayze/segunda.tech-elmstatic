6e182d46987e0963bea7e01a1ad889d2a9d365ba
Marcio Frayze David <marcio.david@serpro.gov.br>
2021-06-10 14:14:39 -0300

    padronizando sed para padrao linux

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf ./_site
 elmstatic build
 
-find . -type f -name "*.html" -print0 | xargs -0 sed -i '' 's/<meta charset="utf-8">/<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">/g'
+find . -type f -name "*.html" -print0 | xargs -0 sed -i 's/<meta charset="utf-8">/<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">/g'
 
 cp _site/rss.xml _site/feedrss.xml
