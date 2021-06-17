80c69eec0df9505a929f1945c6cbebe3f836740b
Marcio Frayze David <marcio.david@serpro.gov.br>
2021-06-17 18:17:09 -0300

    movendo script

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ elmstatic build
 
 find . -type f -name "*.html" -print0 | xargs -0 sed -i 's/<meta charset="utf-8">/<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">/g'
 
+echo "Removendo links na página do curso de Clojure"
+sed -i 's/<a[^>]*>/<b>/g' ./_site/clojure/index.html 
+sed -i 's/<\/a[^>]*>/<\/b>/g' ./_site/clojure/index.html
+
 cp _site/rss.xml _site/feedrss.xml
